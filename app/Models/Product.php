b7bdc410278c6ca1cfa088e1731a72535a879b11
bola-cloud <bola.ishak41@gmail.com>
2024-11-11 23:47:53 +0200

    dsd

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,8 +9,9 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'name',
+        'title',
         'description',
         'price',
+        'image',
     ];
 }
