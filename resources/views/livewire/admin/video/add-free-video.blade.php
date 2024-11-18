2d8501cc79f0885e5f1ce8c9cd6d4c6a2d81f742
bola-cloud <bola.ishak41@gmail.com>
2024-11-18 16:15:44 +0200

    sd

--- resources/views/livewire/admin/video/add-free-video.blade.php
+++ resources/views/livewire/admin/video/add-free-video.blade.php
@@ -9,57 +9,57 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header text-center">
-                        <h5>Upload File</h5>
+                        <h5>رفع فيديو</h5>
                     </div>
 
                     <div class="card-body">
                         <form wire:submit.prevent="store">
                             <div class="mb-3">
-                                <label for="exampleFormControlInput1" class="form-label">Video title</label>
-                                <input type="text" class="form-control" wire:model="name" id="exampleFormControlInput1" placeholder="enter video title">
+                                <label for="exampleFormControlInput1" class="form-label">عنوان الفيديو</label>
+                                <input type="text" class="form-control" wire:model="name" id="exampleFormControlInput1" placeholder="أدخل عنوان الفيديو">
                                 @error('name')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                                <label for="exampleFormControlTextarea1" class="form-label">الوصف</label>
                                 <textarea class="form-control" wire:model="description" id="exampleFormControlTextarea1" rows="3"></textarea>
                                 @error('description')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="exampleFormControlInput1" class="form-label">Video link</label>
-                                <input type="text" class="form-control" wire:model="link" id="exampleFormControlInput1" placeholder="enter video link">
+                                <label for="exampleFormControlInput1" class="form-label">رابط الفيديو</label>
+                                <input type="text" class="form-control" wire:model="link" id="exampleFormControlInput1" placeholder="أدخل رابط الفيديو">
                                 @error('link')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="form-group">
-                                <label for="projectinput5">year school</label>
-                                <select id="projectinput5" name="interested" class="form-control"  wire:model="year_type">
-                                    <option value="none" selected="">choose</option>
-                                    <option value="ONE">ONE</option>
-                                    <option value="TWO">TWO</option>
-                                    <option value="THREE">THREE</option>
+                                <label for="projectinput5">السنة الدراسية</label>
+                                <select id="projectinput5" name="interested" class="form-control" wire:model="year_type">
+                                    <option value="none" selected="">اختر</option>
+                                    <option value="ONE">الأولى</option>
+                                    <option value="TWO">الثانية</option>
+                                    <option value="THREE">الثالثة</option>
                                 </select>
                             </div>
                             <div class="mb-3">
-                                <label class="form-label">Status</label>
+                                <label class="form-label">الحالة</label>
                                 <div>
                                     <input type="radio" wire:model="status" id="active" value="1">
-                                    <label for="active">Active</label>
+                                    <label for="active">نشطة</label>
                                 </div>
                                 <div>
                                     <input type="radio" wire:model="status" id="nonActive" value="0">
-                                    <label for="nonActive">Non-Active</label>
+                                    <label for="nonActive">غير نشطة</label>
                                 </div>
                                 @error('status')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                             <br><br>
-                            <button type="submit" class="btn btn-sm btn-block btn-danger">Upload</button>
+                            <button type="submit" class="btn btn-sm btn-block btn-danger">رفع</button>
                         </form>      
                     </div>
                 </div>
--- resources/views/livewire/admin/video/video-add-controller.blade.php
+++ resources/views/livewire/admin/video/video-add-controller.blade.php
@@ -9,28 +9,28 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header text-center">
-                        <h5>Upload File</h5>
+                        <h5>رفع ملف</h5>
                     </div>
                     <div class="card-body">
                         <form wire:submit.prevent="store">
                             <div class="mb-3">
-                                <label for="exampleFormControlInput1" class="form-label">Video title</label>
-                                <input type="text" class="form-control" wire:model="title" id="exampleFormControlInput1" placeholder="Enter video title">
+                                <label for="exampleFormControlInput1" class="form-label">عنوان الفيديو</label>
+                                <input type="text" class="form-control" wire:model="title" id="exampleFormControlInput1" placeholder="أدخل عنوان الفيديو">
                                 @error('title') <div class="alert alert-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="mb-3">
-                                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                                <label for="exampleFormControlTextarea1" class="form-label">الوصف</label>
                                 <textarea class="form-control" wire:model="description" id="exampleFormControlTextarea1" rows="3"></textarea>
                                 @error('description') <div class="alert alert-danger">{{ $message }}</div> @enderror
                             </div>
                             <div>
-                                <label for="exampleFormControlInput1" class="form-label">Video link</label>
-                                <input type="text" class="form-control" wire:model="link" id="exampleFormControlInput1" placeholder="Enter video link">
+                                <label for="exampleFormControlInput1" class="form-label">رابط الفيديو</label>
+                                <input type="text" class="form-control" wire:model="link" id="exampleFormControlInput1" placeholder="أدخل رابط الفيديو">
                                 @error('link') <div class="alert alert-danger">{{ $message }}</div> @enderror
                             </div>
                             <div class="mt-3">
                                 <select class="form-select" aria-label="Default select example" wire:model="selectedUnit">
-                                    <option selected>choose unit</option>
+                                    <option selected>اختر الوحدة</option>
                                     @foreach ($units as $unit)
                                         <option value="{{ $unit->id }}">{{ $unit->name }}</option>
                                     @endforeach
@@ -38,14 +38,14 @@
                             </div>
                             <div class="mt-3">
                                 <select class="form-select" aria-label="Default select example" wire:model="selectedLecture">
-                                    <option selected>Select lecture</option>
+                                    <option selected>اختر المحاضرة</option>
                                     @foreach ($lectures as $lecture)
                                         <option value="{{ $lecture->id }}">{{ $lecture->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
                             <br><br>
-                            <button type="submit" class="btn btn-sm btn-block btn-danger">Upload</button>
+                            <button type="submit" class="btn btn-sm btn-block btn-success">رفع</button>
                         </form>
                     </div>
                 </div>
