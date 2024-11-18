96af59b3d3d11248b6e8538396f9e8d70e901d9e
bola-cloud <bola.ishak41@gmail.com>
2024-11-19 01:09:45 +0200

    sd

--- resources/views/livewire/admin/slider/slide-add-component.blade.php
+++ resources/views/livewire/admin/slider/slide-add-component.blade.php
@@ -1,80 +1,80 @@
 <div>
-    <style>
-        span.error {
-    color: red;
+  <style>
+      span.error {
+  color: red;
 }
-    </style>
-    <section id="basic-form-layouts">
-        <div class="row match-height">
-          <div class="col-md-12">
-            <div class="card">
-              <div class="card-header">
-                <h4 class="card-title" id="basic-layout-form">Sliders</h4>
-                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-                <div class="heading-elements">
-                  <ul class="list-inline mb-0">
-                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-                    <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-                    <li><a data-action="close"><i class="ft-x"></i></a></li>
-                  </ul>
-                </div>
+  </style>
+  <section id="basic-form-layouts">
+      <div class="row match-height">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4 class="card-title" id="basic-layout-form">السلايدر</h4>
+              <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+              <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                  <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                  <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                  <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                  <li><a data-action="close"><i class="ft-x"></i></a></li>
+                </ul>
               </div>
-              <div class="card-content collapse show">
-                <div class="card-body">
+            </div>
+            <div class="card-content collapse show">
+              <div class="card-body">
 
-                  <form class="form" wire:submit.prevent="addslider">
-                    <div class="form-body">
+                <form class="form" wire:submit.prevent="addslider">
+                  <div class="form-body">
 
-                      <div class="row">
-                        <div class="col-md-6">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label for="projectinput1">عنوان السلايدر</label>
+                          <input type="text" id="projectinput1" class="form-control" placeholder="اسم السلايدر"
+                          name="fname" wire:model="title">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
                           <div class="form-group">
-                            <label for="projectinput1">slider  Title</label>
-                            <input type="text" id="projectinput1" class="form-control" placeholder="Name slider"
-                            name="fname" wire:model="title">
+                            <label for="projectinput5">السنة الدراسية</label>
+                            <select id="projectinput5" name="interested" class="form-control"  wire:model="year_type">
+                              <option value="none" selected="">اختر</option>
+                              <option value="ONE">الأولى</option>
+                              <option value="TWO">الثانية</option>
+                              <option value="THREE">الثالثة</option>
+                            </select>
                           </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                              <label for="projectinput5">year school</label>
-                              <select id="projectinput5" name="interested" class="form-control"  wire:model="year_type">
-                                <option value="none" selected="">choose</option>
-                                <option value="ONE">ONE</option>
-                                <option value="TWO">TWO</option>
-                                <option value="THREE">THREE</option>
-                              </select>
-                            </div>
+                        <div class="col-md-12">
+                          <label class="col-md-3 label-control" for="projectinput1">صورة السلايدر</label>
+                          <div class="col-md-5">
+                            <input type="file" class="form-control"
+                            name="name_company" wire:model="image">
                           </div>
-                          <div class="col-md-12">
-                            <label class="col-md-3 label-control" for="projectinput1"> image slider</label>
-                            <div class="col-md-5">
-                              <input type="file" class="form-control"
-                              name="name_company" wire:model="image">
+                          <div class="col-md-4">
+                                  @if($image)
+                                  <img src="{{$image->temporaryUrl()}}" width="120px">
+                                  @endif
                             </div>
-                            <div class="col-md-4">
-                                    @if($image)
-                                    <img src="{{$image->temporaryUrl()}}" width="120px">
-                                    @endif
-                              </div>
-                          </div>
-
-                      </div>
-
+                        </div>
 
                     </div>
-                    <div class="form-actions">
-                      <a type="button" class="btn btn-warning mr-1" href="{{ route("home_admin") }}">
-                        <i class="ft-x"></i> Cancel
-                      </a>
-                      <button type="submit" class="btn btn-primary">
-                        <i class="la la-check-square-o"></i> Save
-                      </button>
-                    </div>
-                  </form>
-                </div>
+
+
+                  </div>
+                  <div class="form-actions">
+                    <a type="button" class="btn btn-warning mr-1" href="{{ route("home_admin") }}">
+                      <i class="ft-x"></i> إلغاء
+                    </a>
+                    <button type="submit" class="btn btn-primary">
+                      <i class="la la-check-square-o"></i> حفظ
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
+        </div>
 
-      </section>
+    </section>
 </div>
