c70ca06bd9231e0abf6e9b846638df9e942490e5
bola-cloud <bola.ishak41@gmail.com>
2024-12-20 04:28:09 +0200

    slider

--- resources/views/livewire/admin/products/product-manager.blade.php
+++ resources/views/livewire/admin/products/product-manager.blade.php
@@ -73,7 +73,7 @@
 </div>
 
 @push('style')
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> --}}
 @endpush
 
 <script>
