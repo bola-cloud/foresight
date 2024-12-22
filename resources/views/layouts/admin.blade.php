483ef159043d6c5528d6ca334940d500630cdc37
bola-cloud <bola.ishak41@gmail.com>
2024-12-22 23:32:44 +0200

    image

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -151,11 +151,11 @@ data-open="click" data-menu="vertical-content-menu" data-col="2-columns">
       </div>
       <div class="main-menu menu-static menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
         <div class="main-menu-content">
-          <li class=" navigation-header"> 
-            <span data-i18n="nav.category.layouts">ادارة الكورسات و الفيديوهات</span><i class="la la-ellipsis-h ft-minus" data-toggle="tooltip"
-            data-placement="right" data-original-title="Layouts"></i>
-          </li>
           <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+            <li class=" navigation-header"> 
+              <span data-i18n="nav.category.layouts">ادارة الكورسات و الفيديوهات</span><i class="la la-ellipsis-h ft-minus" data-toggle="tooltip"
+              data-placement="right" data-original-title="Layouts"></i>
+            </li>
             <li class=" nav-item"><a href="index.html"><i class="la la-align-right"></i><span class="menu-title" data-i18n="nav.dash.main">الكورسات</span></a>
               <ul class="menu-content">
                 <li class="{{ Route::currentRouteName() == "show_unit" ? 'active':'' }}"><a class="menu-item" href="{{ route("show_unit") }}" data-i18n="nav.dash.ecommerce">عرض الكورسات</a></li>
