28a3b707028648bd1afbc2f093c01a73be74f459
bola-cloud <bola.ishak41@gmail.com>
2024-12-24 13:07:39 +0200

    for

--- app/Http/Livewire/Admin/Lectures/LectureAdd.php
+++ app/Http/Livewire/Admin/Lectures/LectureAdd.php
@@ -16,36 +16,42 @@ class LectureAdd extends Component
 
     public function store()
     {
-        // dd($this->all());
         $validated = $this->validate([
             'name' => 'required|min:3',
             'cost' => 'required|numeric',
             'status' => 'required',
             'image' => 'nullable|max:1024',
-            'unit_id' => 'required|exists:units,id',
             'description' => 'nullable',
         ]);
-        dd($this->all());
-        $new_file = null;
-        if ($this->image) {
-            $filename = $this->image->getClientOriginalName();
-            $this->image->storeAs('', $filename, 'public_lecture');
-            $new_file = 'lecture-images/' . $filename;
+    
+        DB::beginTransaction();
+        try {
+            $new_file = null;
+            if ($this->image) {
+                $filename = $this->image->getClientOriginalName();
+                $this->image->storeAs('', $filename, 'public_lecture');
+                $new_file = 'lecture-images/' . $filename;
+            }
+    
+            $lecture = new Lecture();
+            $lecture->fill([
+                'name' => $this->name,
+                'cost' => 0,
+                'status' => $this->status,
+                'image' => $new_file,
+                'unit_id' => $this->unit_id,
+                'description' => $this->description,
+            ]);
+            $lecture->save();
+    
+            DB::commit();
+            session()->flash('success', 'تم اضافة القسم بنجاح');
+            return redirect()->route('lecture_index');
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            $this->errorMessage = $e->getMessage();
+            Log::error('Error in LectureAdd@store: ' . $e->getMessage());
         }
-
-        $lecture = new Lecture();
-        $lecture->fill([
-            'name' => $this->name,
-            'cost' => 0,
-            'status' => $this->status,
-            'image' => $new_file,
-            'unit_id' => $this->unit_id,
-            'description' => $this->description,
-        ]);
-        $lecture->save();
-
-        session()->flash('success', 'تم اضافة القسم بنجاح');
-        return redirect()->route('lecture_index');
     }
     
 
