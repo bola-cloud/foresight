5437aec05ae03148e540f57be77e70c19f525bcf
bola-cloud <bola.ishak41@gmail.com>
2024-12-30 15:31:43 +0200

    exam

--- resources/views/livewire/admin/video/lecture-videos.blade.php
+++ resources/views/livewire/admin/video/lecture-videos.blade.php
@@ -107,7 +107,7 @@
                         هل أنت متأكد أنك تريد حذف هذا الفيديو؟
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">إلغاء</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">إلغاء</button>
                         <button type="button" class="btn btn-danger" wire:click="deleteVideo">حذف</button>
                     </div>
                 </div>
