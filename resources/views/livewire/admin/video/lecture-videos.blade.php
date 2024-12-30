f4850b92a1cf643688616f165bc866894cfeb976
bola-cloud <bola.ishak41@gmail.com>
2024-12-30 15:29:25 +0200

    exam

--- resources/views/livewire/admin/video/lecture-videos.blade.php
+++ resources/views/livewire/admin/video/lecture-videos.blade.php
@@ -94,7 +94,7 @@
             </div>
         </div>
             <!-- Delete Confirmation Modal -->
-        <div wire:ignore.self class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
