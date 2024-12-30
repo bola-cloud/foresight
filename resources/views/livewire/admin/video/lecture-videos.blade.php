f41c7936adfbdadb155b57ea99234868714730f4
bola-cloud <bola.ishak41@gmail.com>
2024-12-30 14:41:27 +0200

    exam

--- resources/views/livewire/admin/video/lecture-videos.blade.php
+++ resources/views/livewire/admin/video/lecture-videos.blade.php
@@ -103,7 +103,6 @@
         <script src="https://cdn.plyr.io/3.6.8/plyr.polyfilled.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@livewire/livewire"></script>
         <script>
             document.addEventListener('livewire:load', () => {
                 Livewire.on('playVideo', (event) => {
